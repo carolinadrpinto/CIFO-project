21f9fd2aa54c3f7a3166bb5f35ceb1a298c5c899
cribe21 <catarina.ribeirinha@hotmail.com>
2025-05-23 23:40:03 +0100

    Add files via upload

--- library/selection_and_operators/selection.py
+++ library/selection_and_operators/selection.py
@@ -10,7 +10,7 @@ def ranking_selection(population: list[Solution], maximization: bool, s=1.5):
     fitness_values= [ind.fitness() for ind in population]
 
     if maximization:
-        ranking = sorted(zip(population, fitness_values), key=lambda x: x[1], reverse=True)
+        ranking = sorted(zip(population, fitness_values), key=lambda x: x[1], reverse=True) #sort individuals by highest fitness
     else:
         # Minimization
         ranking = sorted(zip(population, fitness_values), key=lambda x: x[1], reverse=False)
@@ -19,25 +19,25 @@ def ranking_selection(population: list[Solution], maximization: bool, s=1.5):
     #it can be tuned s
     ranks_array = np.arange(population_size)
     print('ranks_array:', ranks_array)
-    probabilities = (2 - s)/population_size + (2 * ranks_array * (s - 1)) / (population_size * (population_size - 1))
+    probabilities = (2 - s)/population_size + (2 * ranks_array * (s - 1)) / (population_size * (population_size - 1)) #linear probability formula (selection probabilities)
     print('prob1:', probabilities)
     probabilities = probabilities[::-1]  # reverse so that the the individual with rank 0 gets the highest probability of being chosen
     print('prob2:', probabilities)
 
-    selected_indices = np.random.choice(population_size, size=population_size, p=probabilities)
+    selected_indices = np.random.choice(population_size, size=population_size, p=probabilities) #randomly select the parents according to the probabilities
     print('selected_indexs:', selected_indices)
 
     parents = [deepcopy(ranking[i][0]) for i in selected_indices]
-    best=parents[0]
+    best=parents[0] #select the best one
 
     return deepcopy(best)
         
 
 def tournament_selection(population: list[Solution], maximization: bool, k=2): #30 to 40 % moderated pressure
     
-        tournament_participants = np.random.choice(population, size=k, replace=False)        
+        tournament_participants = np.random.choice(population, size=k, replace=False)  #select randomly k participants for the tournament   
         if maximization:
-            best = max(tournament_participants, key=lambda ind: ind.fitness())
+            best = max(tournament_participants, key=lambda ind: ind.fitness()) #select the best one based on the fitness
         else:
             best = min(tournament_participants, key=lambda ind: ind.fitness())
             
